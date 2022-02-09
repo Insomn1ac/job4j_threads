4bfed12da5e71d326b9fa3287ee821ae0c67c7c2
Insomn1ac <emelyanoff94@gmail.com>
2022-02-09 21:10:36 +0300

    Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -0,0 +1,34 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenOneThreadIsProducerAndAnotherIsConsumer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    Thread.sleep(250);
+                    queue.offer(i);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 7; i++) {
+                queue.poll();
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertEquals(3, queue.size());
+        assertEquals(Integer.valueOf(7), queue.poll());
+    }
+}
