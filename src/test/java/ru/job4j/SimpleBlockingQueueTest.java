f0de7f743e1f6d0384e29ecf0609150c144df464
Insomn1ac <emelyanoff94@gmail.com>
2022-02-12 20:48:30 +0300

    Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -2,6 +2,9 @@ package ru.job4j;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
@@ -10,29 +13,61 @@ public class SimpleBlockingQueueTest {
     public void whenOneThreadIsProducerAndAnotherIsConsumer() throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
         Thread producer = new Thread(() -> {
-            try {
-                for (int i = 0; i < 10; i++) {
-                    Thread.sleep(250);
-                    queue.offer(i);
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        });
-        Thread consumer = new Thread(() -> {
-            for (int i = 0; i < 7; i++) {
+            for (int i = 0; i < 10; i++) {
                 try {
-                    queue.poll();
+                    queue.offer(i);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
             }
         });
         producer.start();
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 7; i++) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
         consumer.start();
         producer.join();
+        consumer.interrupt();
         consumer.join();
         assertEquals(3, queue.size());
         assertEquals(Integer.valueOf(7), queue.poll());
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    queue.offer(i);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        producer.start();
+        Thread consumer = new Thread(() -> {
+            while (queue.size() != 0 || !Thread.currentThread().isInterrupted()) {
+                try {
+                    buffer.add(queue.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(buffer, Arrays.asList(0, 1, 2, 3, 4));
     }
 }
